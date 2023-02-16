5fdf8320bdf7ca026063cdf04952a4603bc0e635
asaladi <122575267+asaladi@users.noreply.github.com>
2023-02-16 15:47:20 -0800

    Update grade.sh

--- grade.sh
+++ grade.sh
@@ -1,5 +1,35 @@
-CPATH='.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar'
+CPATH='.;../lib/junit-4.13.2.jar;../lib/hamcrest-core-1.3.jar'
 
 rm -rf student-submission
 git clone $1 student-submission
 echo 'Finished cloning'
+
+cd student-submission/
+
+if [ -f "ListExamples.java" ]
+then
+    echo "ListExamples found"
+else
+    echo "need file ListExamples.java"
+    exit 1
+fi
+
+cd ..
+
+rm -rf test-submission
+mkdir test-submission
+cp student-submission/*.java test-submission
+cp *.java test-submission
+echo "it copied successfully"
+
+
+cd test-submission
+
+javac -cp $CPATH *.java
+if [[ $? -ne 0 ]]
+then 
+    echo "involving javac-error.txt"
+    exit 1
+fi
+
+java -cp $CPATH org.junit.runner.JUnitCore TestListExamples
